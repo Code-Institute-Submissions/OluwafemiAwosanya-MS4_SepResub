bd6243ad0a4b802007d50bd75cd1a79d0ae05904
OluwafemiAwosanya <oluwafemiawosanya@hotmail.com>
2021-07-30 00:41:17 +0000

    Deployment-disable collectstatic

--- ms4/settings.py
+++ ms4/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1!)(_)dpknl9gyqeurn&l3&7wh@6g%g#3-dv_bxe1sra-15jr%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-retail.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,13 +123,17 @@ WSGI_APPLICATION = 'ms4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-     'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': BASE_DIR / 'db.sqlite3',
-  }
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
