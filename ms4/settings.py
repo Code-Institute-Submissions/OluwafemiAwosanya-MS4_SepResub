4342d357c1901207da8c6923ac645036d1157da1
OluwafemiAwosanya <oluwafemiawosanya@hotmail.com>
2021-09-01 19:05:47 +0000

    sorting out migration issue

--- ms4/settings.py
+++ ms4/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = True
 
 ALLOWED_HOSTS = ['ms4-retail.herokuapp.com', 'localhost']
@@ -136,7 +136,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 
 # Password validation
